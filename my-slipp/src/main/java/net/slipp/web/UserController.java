55d5dbf8a2322a3d1fcbcc302fc027b85338fb43
gdkor <gdkor@LEBRONPARKSJ>
2018-12-06 18:05:16 +0900

    12월6일 테스트

---
+++ my-slipp/src/main/java/net/slipp/web/UserController.java
@@ -0,0 +1,15 @@
+package net.slipp.web;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+	
+	@RequestMapping("/create")
+	public String Create(User user)
+	{
+		System.out.println("userId : " + user.getUserId() + ", email : " + user.getEmail() + ", name : " + user.getName());
+		return "index";
+	}
+
+}
