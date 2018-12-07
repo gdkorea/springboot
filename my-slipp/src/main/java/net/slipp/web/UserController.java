19f81e866ddbb93a5610b37ec127d10402f8082a
gdkor <gdkor@LEBRONPARKSJ>
2018-12-07 16:30:07 +0900

    12월 7일

--- my-slipp/src/main/java/net/slipp/web/UserController.java
+++ my-slipp/src/main/java/net/slipp/web/UserController.java
@@ -1,15 +1,29 @@
 package net.slipp.web;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
+	private List<User> users = new ArrayList<User>();
 	
 	@RequestMapping("/create")
 	public String Create(User user)
 	{
 		System.out.println("userId : " + user.getUserId() + ", email : " + user.getEmail() + ", name : " + user.getName());
-		return "index";
+		users.add(user);
+		return "redirect:/list";
+	}
+	
+	@GetMapping("/list")
+	public String List(Model model)
+	{
+		model.addAttribute("users",users);
+		return "list";
 	}
 
 }
